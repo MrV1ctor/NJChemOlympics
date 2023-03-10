9fda30a7eaf8c9a36b7a4ffb07d2cbde335499be
CasualQuasar <93143332+Quasardilla@users.noreply.github.com>
2023-03-10 18:13:40 -0500

    hehehehe

---
+++ project/.routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2023-03-10T23:11:55.528Z"
+}
---
+++ project/.routify/routes.js
@@ -0,0 +1,102 @@
+
+/**
+ * @roxi/routify 2.18.11
+ * File generated Fri Mar 10 2023 18:12:56 GMT-0500 (Eastern Standard Time)
+ */
+
+export const __version = "2.18.11"
+export const __timestamp = "2023-03-10T23:12:56.975Z"
+
+//buildRoutes
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+
+//imports
+
+
+//options
+export const options = {}
+
+//tree
+export const _tree = {
+  "name": "_layout",
+  "filepath": "/_layout.svelte",
+  "root": true,
+  "ownMeta": {
+    "preload": "proximity"
+  },
+  "absolutePath": "C:/Users/Casual Quasar/Desktop/Essential_Files/Website_Making/NJChemOlympics/project/src/pages/_layout.svelte",
+  "children": [
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "index.svelte",
+      "filepath": "/index.svelte",
+      "name": "index",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "C:/Users/Casual Quasar/Desktop/Essential_Files/Website_Making/NJChemOlympics/project/src/pages/index.svelte",
+      "importPath": "../src/pages/index.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": true,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": "proximity",
+        "prerender": true
+      },
+      "path": "/index",
+      "id": "_index",
+      "component": () => import('../src/pages/index.svelte').then(m => m.default)
+    },
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "testing.svelte",
+      "filepath": "/testing.svelte",
+      "name": "testing",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "C:/Users/Casual Quasar/Desktop/Essential_Files/Website_Making/NJChemOlympics/project/src/pages/testing.svelte",
+      "importPath": "../src/pages/testing.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": false,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "recursive": true,
+        "preload": "proximity",
+        "prerender": true
+      },
+      "path": "/testing",
+      "id": "_testing",
+      "component": () => import('../src/pages/testing.svelte').then(m => m.default)
+    }
+  ],
+  "isLayout": true,
+  "isReset": false,
+  "isIndex": false,
+  "isFallback": false,
+  "isPage": false,
+  "isFile": true,
+  "file": "_layout.svelte",
+  "ext": "svelte",
+  "badExt": false,
+  "importPath": "../src/pages/_layout.svelte",
+  "meta": {
+    "preload": "proximity",
+    "recursive": true,
+    "prerender": true
+  },
+  "path": "/",
+  "id": "__layout",
+  "component": () => import('../src/pages/_layout.svelte').then(m => m.default)
+}
+
+
+export const {tree, routes} = buildClientTree(_tree)
+
