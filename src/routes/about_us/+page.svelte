35520174c8fa26fab05602ccb42c11c3a4e98c48
MrV1ctor <vurumov25@pascack.org>
2023-04-14 09:55:38 -0400

    map shit

--- src/routes/about_us/+page.svelte
+++ src/routes/about_us/+page.svelte
@@ -1,1 +1,12 @@
-<h1>this is the about us page</h1>
+<script>
+    import Map from '../../sections/map.svelte'
+</script>
+
+<main>
+    <h1>this is the about us page</h1>
+    <Map/>
+</main>
+
+<style>
+
+</style>
