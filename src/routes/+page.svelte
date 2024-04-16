dcaa81e54ce7f0d8a37b898de2c395c161293260
Quasardilla <casualquasar@gmail.com>
2024-04-16 18:05:29 -0400

    fixed main page citations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -242,14 +242,15 @@
     
         var acc = document.getElementsByClassName("accordion");
   
-        // acc[0].addEventListener("click", function() {
-        //     this.classList.toggle("active");
-        //     var text = this.nextElementSibling;
-        //     if (text.style.maxHeight) {
-        //     text.style.maxHeight = null;
-        //     } else {
-        //     text.style.maxHeight = text.scrollHeight + "px";
-        //     } 
+        acc[0].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var text = this.nextElementSibling;
+          if (text.style.maxHeight) {
+          text.style.maxHeight = null;
+          } else {
+          text.style.maxHeight = text.scrollHeight + "px";
+          } 
+        });
         
         // });
 
